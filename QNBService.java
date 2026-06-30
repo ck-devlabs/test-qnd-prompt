a4eac05aa472eb0d41f6ff6f5c1cf45650752bc9
ck-devlabs <116240301+ck-devlabs@users.noreply.github.com>
2026-06-30 13:29:05 -0400

    Update QNBService.java

--- QNBService.java
+++ QNBService.java
@@ -1,1 +1,6 @@
 
+Map<String, Object> jobMap = jobParams.toMap();
+jobMap.put("storedFile.path", storedFileInfo.getPath());
+jobMap.put("storedFile.fileName", storedFileInfo.getFileName());
+jobMap.put("storedFile.size", storedFileInfo.getSize());
+jobMap.put("storedFile.mimeType", storedFileInfo.getMimeType());
